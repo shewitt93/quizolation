8c34d978dde8d8cf5168ece41e3b44e85f3b883f
hannahtesfay <62282069+hannahtesfay@users.noreply.github.com>
2020-08-21 11:12:01 +0100

    add colons to separate scores and player

--- src/container/ResultsPage.js
+++ src/container/ResultsPage.js
@@ -14,9 +14,9 @@ class ResultsPage extends Component {
         <h1>Results</h1>
 
 
-        <div className="resultPlacement">🥇 {this.props.score[0].score1} Player 1 🥇</div>
-        <div className="resultPlacement">🥈 {this.props.score[1].score2} Player 2 🥈</div>
-        <div className="resultPlacement">🥉 {this.props.score[3].score4} Player 3 🥉</div>
+        <div className="resultPlacement">🥇 {this.props.score[0].score1}: Player 1 🥇</div>
+        <div className="resultPlacement">🥈 {this.props.score[1].score2}: Player 2 🥈</div>
+        <div className="resultPlacement">🥉 {this.props.score[3].score4}: Player 3 🥉</div>
         <div>{this.state.userScore}</div>
         
         <NavLink to="/homepage" className="playAgain">Play again?</NavLink>
